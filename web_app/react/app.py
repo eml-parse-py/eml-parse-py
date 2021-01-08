20fc22e26fb778d0c4710cb4b72cbde4ac9757d8
Mark McMoran <mark.mcmoran@live.com>
2021-01-08 02:04:55 +0000

    Some Flask logic inside this file

--- web_app/react/app.py
+++ web_app/react/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./app/static/parse_py_ui/build', static_url_path='/')
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return app.send_static_file('index.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
