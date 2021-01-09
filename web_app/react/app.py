c922a2274c196d1c58cf014c26a8b168f1b47f05
Mark McMoran <mark.mcmoran@live.com>
2021-01-09 22:55:38 +0000

    Upload logic, and update to gitignore for eml files

--- web_app/react/app.py
+++ web_app/react/app.py
@@ -1,15 +1,14 @@
 from flask import Flask, request
 from flask_cors import CORS
-
+from eml_api import extract_header
 app = Flask(__name__, static_folder='./app/static/parse_py_ui/build', static_url_path='/')
 CORS(app)
 
 
 @app.route("/uploadfile", methods=["POST"])
 def handle_file_upload():
-    file = request.form.to_dict()
-    for value in file.values():
-        file = value
+    file = request.files['file'].save(dst="message.eml")
+    print(f"the type is: {file}")
     return f"{file}"
 
 
