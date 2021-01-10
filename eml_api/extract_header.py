1b8aa862fd34bd4773f63bcad7733cfa0bfc272e
Mark McMoran <mark.mcmoran@live.com>
2021-01-10 17:03:20 +0000

    Code clean up

--- eml_api/extract_header.py
+++ eml_api/extract_header.py
@@ -11,41 +11,17 @@ from email.policy import default
 
 class ExtractHeader:
 
-    _crlf = r'\r\n'
-    _lf = r'\n'
-
     def open_msg_file(self, msg):
         with open(msg, 'rb') as file:
             headers = BytesParser(policy=default).parse(file)
         return headers
 
-    def msg_header_from(self, msg):
+    def ret_header(self, msg, val):
         _opened = self.open_msg_file(msg)
-        _opened.get('from')
+        _opened.get(val)
         return _opened
 
-    def msg_header_to(self, msg):
+    def header_gen(self, msg):
         _opened = self.open_msg_file(msg)
-        _opened.get('to')
-        return _opened
-
-    def msg_header_subject(self, msg):
-        _opened = self.open_msg_file(msg)
-        _opened.get('subject')
-        return _opened
-
-    def msg_header_reply_to(self, msg):
-        _opened = self.open_msg_file(msg)
-        _opened.get('reply-to')
-        return _opened
-
-    def msg_header_msg_id(self, msg):
-        _opened = self.open_msg_file(msg)
-        _opened.get('message-id')
-        return _opened
-
-    def msg_header_date(self, msg):
-        _opened = self.open_msg_file(msg)
-        _opened.get('date')
-        return _opened
-
+        headers = str(_opened.keys()) + " " + str(_opened.items())
+        return headers
