ecc500f0ff070b59e48fa1039276e8889d2eca7d
Mark McMoran <mark.mcmoran@live.com>
2021-02-01 23:52:47 +0000

    Further cleaning of dir struct

---
+++ frontend/src/form/App.js
@@ -0,0 +1,13 @@
+import '../App.css';
+import React from 'react';
+import Form from './form';
+function App() {
+  return (
+    <div className="App">
+        <h2> Eml Parse Py</h2>
+    <Form />
+     </div>
+  );
+}
+
+export default App;
