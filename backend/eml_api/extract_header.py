fd0ebdc7522dab853d6f41f556a26a9a5ff8d2f7
Mark McMoran <mark.mcmoran@live.com>
2021-06-10 13:24:45 +0100

    Email did not contain header information
    What was changed?
    
    Extract_header.py - Removed the .MSG file handling references, this will be dealt with
    in Issue 32 on GH.
    
    message_details - Removed the for loop, and replaced the msg_data
    template variable with the message_data one used in Extract_header.py

--- backend/eml_api/extract_header.py
+++ backend/eml_api/extract_header.py
@@ -21,14 +21,6 @@ class ExtractHeader:
     def __init__(self, msg_file_path):
         self.msg_file_path = msg_file_path
         self.msg_extension = msg_file_path.rsplit('.', 1)[1].lower()
-        try:
-            self.message_file_object = "MsOxMessage(msg_file_path)"
-        except:
-            print("This file wasn't a .MSG file.")
-
-    def parse_msg_upload(self):
-
-        return ""
 
     def parse_eml_upload(self):
         with open(self.msg_file_path, "rb") as file:
@@ -41,15 +33,15 @@ class ExtractHeader:
         if self.msg_extension == "eml":
             return self.parse_eml_upload()
         else:
-            return self.parse_msg_upload()
+            # Issue 32 on Github will add MSG support
+            pass
 
     def handle_saved_message_data(self):
         if self.msg_extension == "eml":
             eml_json = self.parse_eml_upload()
             self.save_message_json(eml_json)
         else:
-            # msg_json = self.parse_msg_upload()
-            # self.save_message_json(msg_json)
+            # Issue 32 on Github will add MSG support
             pass
 
     def timestamped_file_format(self):
@@ -59,7 +51,6 @@ class ExtractHeader:
 
     def save_message_json(self, json_data):
         filename = self.timestamped_file_format()
-        print(f"{filename} is the name of this file..\n\n\n\n")
         with open(filename, "w") as file:
             json.dump(json_data, file)
 
@@ -73,8 +64,9 @@ class ExtractHeader:
         directory_of_script = 'eml_api/'
         jinja_env_variable = jinja2.Environment(loader=jinja2.FileSystemLoader(f"{directory_of_script}templates"))
         template_file = jinja_env_variable.get_template('message_details.html')
+        msg_data = self.get_message_data_json()
         jinja_dictionary = {
-            'message_headers': self.get_message_data_json()
+            'message_headers': msg_data
         }
         craft_html = template_file.render(jinja_dictionary)
         with open(f"{directory_of_script}AnalysedHeaders.html", "w") as file:
